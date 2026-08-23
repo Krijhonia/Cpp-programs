ee936bacd5270276a68173fc929f930d15bee7f7
Krijhonia <krijhonia@users.noreply.github.com>
2026-08-23 14:05:09 +0530

    Some cpp programs

---
+++ fact.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int factorial(int num);
+int main(){
+
+    std::cout<< factorial(10);
+    return 0;
+}
+int factorial(int num){
+    //Iterative approach
+    // int result = 1;
+    // for(int i = 1;  i<=num;i++){
+    //     result =  result * i;
+    // }
+    // return result;
+
+    //Recursive  approach
+    if(num>1){
+        return num * factorial(num-1);
+
+    }
+    else{
+        return 1;
+    }
+}
---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void walk(int steps);
+
+int main(){
+    walk(10);
+    return 0;
+}
+void walk(int steps){
+    if(steps>0){
+        std::cout << "You take a step!\n";
+        walk(steps-1);
+    }
+}
